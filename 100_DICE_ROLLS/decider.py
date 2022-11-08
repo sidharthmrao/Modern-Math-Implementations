72bba8c4df5511c2c2b6c208acd50f811e96aaf5
Sidharth Rao <sidharthmrao@gmail.com>
2022-11-08 18:23:14 -0500

    comments

--- 100_DICE_ROLLS/decider.py
+++ 100_DICE_ROLLS/decider.py
@@ -1,12 +1,19 @@
 from roll_sim import generate_rolls, rolls_set
 
 ITERS = 100
-SIDES = 6
+SIDES = 10
 TESTS = 100000
 
-worked = 0
+successful = 0
 
 for i in range(TESTS):
+    """
+    Assume that every dice roll adds up to a number divisible by 6 (SUM % 6 = 0).
+    Everyone selects a guess that would satisfy that condition.
+    1/6 correct solve rate because there is a 1/6 chance of everything adding to mod 6.
+    1/6 is the maximum probability because that is the probability of one dice roll working.
+    """
+
     rolls = generate_rolls(ITERS, SIDES)
     counts = rolls_set(rolls, SIDES)
 
@@ -21,6 +28,6 @@ for i in range(TESTS):
     # print(decisions)
 
     if rolls == decisions:
-        worked += 1
+        successful += 1
 
-print(worked/TESTS)
+print(f"Success rate over {TESTS} tests: {successful/TESTS} ({successful} rounds successful).")
